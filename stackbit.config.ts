b72dc2246928cc5b85572d1eaefc1933df42d364
Jacqueline Mhitaryan <jackie@mhitaryan.com>
2025-10-05 16:21:32 +0200

    Configure Stackbit GitContentSource for Visual Editor

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,28 +1,37 @@
-import { defineStackbitConfig } from '@stackbit/types';
 import { GitContentSource } from '@stackbit/cms-git';
-import { allModels } from './.stackbit/models';
+import { defineStackbitConfig } from '@stackbit/types';
 
-const config = defineStackbitConfig({
-    stackbitVersion: '~0.7.0',
-    ssgName: 'nextjs',
-    nodeVersion: '18',
-    contentSources: [
-        new GitContentSource({
-            rootPath: __dirname,
-            contentDirs: ['content'],
-            models: allModels,
-            assetsConfig: {
-                referenceType: 'static',
-                staticDir: 'public',
-                uploadDir: 'images',
-                publicPath: '/'
-            }
-        })
-    ],
-    presetSource: {
-        type: 'files',
-        presetDirs: ['./.stackbit/presets']
-    },
-    styleObjectModelName: 'ThemeStyle'
+export default defineStackbitConfig({
+  stackbitVersion: '~0.7.0',
+  ssgName: 'nextjs',
+  nodeVersion: '18',
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ['content'],
+      models: [
+        {
+          name: 'Page',
+          type: 'page',
+          urlPath: '/{slug}',
+          filePath: 'content/pages/{slug}.md',
+          fields: [
+            { name: 'title', type: 'string', required: true },
+            { name: 'body', type: 'markdown' }
+          ]
+        }
+      ],
+      assetsConfig: {
+        referenceType: 'static',
+        staticDir: 'public',
+        uploadDir: 'images',
+        publicPath: '/'
+      }
+    })
+  ],
+  presetSource: {
+    type: 'files',
+    presetDirs: ['./.stackbit/presets']
+  },
+  styleObjectModelName: 'ThemeStyle'
 });
-export default config;
